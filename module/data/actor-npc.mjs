1d7d2782d155d68fa4a276b8d7b50ac6fbc08109
MrTheBino <mrthebino@gmail.com>
2025-08-10 13:52:36 +0200

    added basic pc sheet

--- module/data/actor-npc.mjs
+++ module/data/actor-npc.mjs
@@ -9,6 +9,10 @@ export default class DieseldrachenNPC extends DieseldrachenActorBase {
 
     schema.cr = new fields.NumberField({ ...requiredInteger, initial: 1, min: 0 });
     schema.xp = new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 });
+
+    schema.species = new fields.StringField({ required: true, nullable: false, initial: "" });
+    schema.dice = new fields.NumberField({ required: true, nullable: false, integer: true, initial: 1, min: 1, max: 100 });
+    schema.armor = new fields.NumberField({ required: true, nullable: false, integer: true, initial: 1, min: 1, max: 100 });
     
     return schema
   }
