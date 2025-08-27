93b8d97808a03cb5b9ef3f1239cc9e6784ad1dc5
MrTheBino <mrthebino@gmail.com>
2025-08-27 08:47:07 +0200

    fix for initial speed value of vehicles

--- module/data/actor-vehicle.mjs
+++ module/data/actor-vehicle.mjs
@@ -31,7 +31,7 @@ export default class DieseldrachenVehicle extends DieseldrachenActorBase {
 
 
      schema.speed = new fields.SchemaField({
-      value: new fields.NumberField({ ...requiredInteger, initial: 1, min: 0 }),
+      value: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 }),
       max: new fields.NumberField({ ...requiredInteger, initial: 1, min: 1 }),
     })
 
